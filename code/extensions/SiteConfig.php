379abcf846392425d84d07fe6e3dcd1ea5d03648
Rhym <contact@ryanpotter.co.nz>
2015-07-08 12:13:24 +1200

    Added permission check against changing the CMS logo

--- code/extensions/SiteConfig.php
+++ code/extensions/SiteConfig.php
@@ -2,6 +2,8 @@
 
 /**
  * Class CMSBrandingSiteConfigExtension
+ *
+ * @method Image CMSLogo
  */
 class CMSBrandingSiteConfigExtension extends DataExtension {
 
@@ -13,28 +15,33 @@ class CMSBrandingSiteConfigExtension extends DataExtension {
      * @param FieldList $fields
      */
     public function updateCMSFields(FieldList $fields) {
-
         /** =========================================
-         * Settings
+         * @var UploadField $cmsLogo
         ===========================================*/
 
+        /** -----------------------------------------
+         * Settings
+        -------------------------------------------*/
+
         if (!$fields->fieldByName('Root.Settings')){
-            $fields->addFieldToTab('Root', new TabSet('Settings'));
+            $fields->addFieldToTab('Root', TabSet::create('Settings'));
         }
 
         /** -----------------------------------------
          * CMS
         -------------------------------------------*/
 
-        $fields->findOrMakeTab('Root.Settings.CMS', 'CMS');
-        $fields->addFieldsToTab('Root.Settings.CMS',
-            array(
-                new HeaderField('Images'),
-                $cmsLogo = new UploadField('CMSLogo', 'Logo (optional)')
-            )
-        );
-        $cmsLogo->setFolderName('Uploads/CMS');
-        $cmsLogo->setRightTitle('Logo displayed in the top left-hand side of the CMS menu. Rescaled to a height of 50px.');
+        if (Permission::check('ADMIN')) {
+            $fields->findOrMakeTab('Root.Settings.CMS', 'CMS');
+            $fields->addFieldsToTab('Root.Settings.CMS',
+                array(
+                    HeaderField::create('Images'),
+                    $cmsLogo = UploadField::create('CMSLogo', 'Logo (optional)')
+                )
+            );
+            $cmsLogo->setFolderName('Uploads/CMS');
+            $cmsLogo->setRightTitle('Logo displayed in the top left-hand side of the CMS menu. Rescaled to a height of 50px.');
+        }
 
     }
 
