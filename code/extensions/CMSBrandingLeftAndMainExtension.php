2cfbdd495f245230a91644289ed35778ae69fcb0
Rhym <contact@ryanpotter.co.nz>
2015-07-29 12:23:42 +1200

    Changed ->stat to ->config()

--- code/extensions/CMSBrandingLeftAndMainExtension.php
+++ code/extensions/CMSBrandingLeftAndMainExtension.php
@@ -6,13 +6,16 @@
 class CMSBrandingLeftAndMainExtension extends LeftAndMainExtension
 {
 
+    /**
+     * @var string
+     */
     private static $cms_menu_background = '#1b354c';
 
     public function init()
     {
         /** Set the background colour of the cms sidebar menu. */
         Requirements::customCSS(
-            '.cms-menu {background: ' . $this->owner->stat('cms_menu_background') . ';}'
+            '.cms-menu {background: ' . $this->owner->config()->cms_menu_background . ';}'
         );
     }
 
