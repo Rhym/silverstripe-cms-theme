dd2f6b5e62cc7b59254a7a199054aa31411bd789
Rhym <contact@ryanpotter.co.nz>
2017-03-02 12:53:12 +0000

    Updated styles in line with 4.0

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
